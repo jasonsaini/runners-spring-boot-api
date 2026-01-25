0376d16020ee6f39889398eae621df8c43781392
Jason Saini <jasonsaini.exe@gmail.com>
2026-01-24 23:15:49 -0500

    Add starting logic for REST methods

---
+++ src/main/java/dev/jasonsaini/runners/run/RunRepository.java
@@ -0,0 +1,45 @@
+package dev.jasonsaini.runners.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class RunRepository {
+
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll ()
+    {
+        return runs;
+    }
+
+    @PostConstruct // used on a method that needs to be executed post dependency-injections.
+    private void init(){
+        runs.add(new Run(1,
+                "Monday Evening Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+                3,
+                Location.INDOOR));
+
+        runs.add(new Run(2,
+                "Tuesday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+                6,
+                Location.INDOOR));
+
+    }
+
+    Optional<Run> findById(Integer id){
+        return runs.stream()
+                .filter(run -> run.id() == id)
+                .findFirst();
+    }
+}
